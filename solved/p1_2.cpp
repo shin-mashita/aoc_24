44321cd15e6f5503a4e2c688dfa9a0b37449de0a
Cedric Encarnacion <dakarashin0@gmail.com>
2024-12-05 06:08:46 +0800

    Solved day 2.2

--- solved/p1_2.cpp
+++ solved/p1_2.cpp
@@ -3,11 +3,13 @@
 #include <string>
 #include <cstring>
 #include <vector>
+#include <unordered_set>
+#include <set>
 #include <algorithm>
 #include <math.h>
 
 #define ll long long
-#define FOR(_i,_n) for(int _i; _i < _n; i++)
+#define FOR(_i,_n) for(int _i = 0; _i < _n; _i++)
 
 /* Debuggers */
 #define showstrvec(svec)		for(string s:svec){cout << s << endl;}
@@ -16,6 +18,26 @@
 using namespace std;
 
 int main(void)
-{	
+{
+	ll u[1000], v[1000];
+	ll count = 0;
+	ll ans = 0;
+
+	FOR(i, 1000) {
+		cin >> u[i] >> v[i];
+	}
+
+	FOR(j, 1000) {
+		FOR(k, 1000) {
+			if (u[j] == v[k])
+				count++;
+		}
+
+		ans += u[j] * count;
+		count = 0;
+	}
+
+	cout << ans;
+
 	return 0;
 }
