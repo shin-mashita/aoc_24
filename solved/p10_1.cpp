cb20aced8d09b021c98737e2035970382c41f2b4
Cedric Encarnacion <dakarashin0@gmail.com>
2024-12-11 03:54:04 +0800

    Solved day 10

---
+++ solved/p10_1.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <sstream>
+#include <set>
+
+#define ll long long
+#define FOR(_i,_n) for(int _i = 0; _i < _n; _i++)
+#define LOG(_x) cout << _x << endl
+
+/* Debuggers */
+#define showvecline(svec)		{for(auto s:svec){cout << s << " ";} cout << endl;}
+#define showvec(vec)			for(auto v:vec){cout << v << endl;}
+
+using namespace std;
+
+#define MAP_SIZE	53
+
+int mmap[MAP_SIZE][MAP_SIZE]; //mmap(y,x)
+set <vector <int>> pos;
+int scores = 0;
+
+static inline vector <ll> _tokenize(string s, char del) //todo: make template
+{
+	vector <ll> v;
+	stringstream ss(s);
+	string word;
+	while (!ss.eof()) {
+		getline(ss, word, del);
+		v.push_back(stoll(word));
+	}
+
+	return v;
+}
+
+void dfs(int val, int posy, int posx) {
+	if (val == 9){
+		vector<int> tempv;
+		tempv.push_back(posy);
+		tempv.push_back(posx);
+		pos.insert(tempv);
+		return;
+	}
+
+	if(posy - 1 >= 0) {
+		if (mmap[posy-1][posx] == val + 1) {
+			dfs(val + 1, posy-1, posx);
+		}
+	}
+	if(posx + 1 < MAP_SIZE) {
+		if (mmap[posy][posx+1] == val + 1) {
+			dfs(val + 1, posy, posx+1);
+		}
+	}
+	if(posy + 1 < MAP_SIZE) {
+		if (mmap[posy+1][posx] == val + 1) {
+			dfs(val + 1, posy+1, posx);
+		}
+	}
+	if(posx - 1 >= 0) {
+		if (mmap[posy][posx-1] == val + 1) {
+			dfs(val + 1, posy, posx-1);
+		}
+	}
+	return;
+}
+
+int main(void)
+{	
+	string s;
+	FOR(i, MAP_SIZE) {
+		getline(cin, s);
+		FOR(j, MAP_SIZE) {
+			mmap[i][j] = stoi(string(1,s[j]));
+		}
+	}
+
+	FOR(i, MAP_SIZE){
+		FOR(j, MAP_SIZE){
+			if(mmap[i][j] == 0){
+				dfs(0, i, j);
+				scores+=pos.size();
+				pos.clear();
+			}
+		}
+	}
+
+	// FOR(i, MAP_SIZE)
+	// 	showvecline(mmap[i]);
+	cout << scores;
+}
